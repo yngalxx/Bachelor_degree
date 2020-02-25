9b7da5770cfbe1033579a254b7ffc7d1befeff57
Alex Drożdż <45334449+yngalxx@users.noreply.github.com>
2020-02-25 13:39:43 +0100

    Create sofifaScraper.py

---
+++ sofifaScraper.py
@@ -0,0 +1,36 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+import pandas as pd
+import time
+
+opts = Options()
+browser = Firefox(options=opts)
+
+browser.get('https://sofifa.com')
+
+search_player_field = browser.find_element_by_name("keyword")
+
+names = pd.read_csv('/Users/alexdrozdz/PycharmProjects/doLicka/nazwiska_zawodnikow.csv')
+names['Age'] = ""
+
+
+for row in names.index:
+    search_player_field.send_keys(names['Player'][row])
+    search_player_field.submit()
+    time.sleep(1)
+
+    try:
+        age = browser.find_element_by_css_selector(
+            '.table > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(3)'
+        ).text
+
+    except:
+        age = 'lack'
+
+    print(names['Player'][row], age)
+    names.loc[row, 'Age'] = age
+    #print(names)
+    search_player_field.clear()
+    time.sleep(1)
+
+names.to_csv('/Users/alexdrozdz/PycharmProjects/doLicka/wiek_zawodnikow.csv')
